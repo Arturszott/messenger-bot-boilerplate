5f7cf8a63080310d6661af03f3c4bdbce63896d2
Artur Szott <a.bartimeus@gmail.com>
2016-05-26 17:01:07 +0200

    remove logs

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,6 @@ export default () => {
 
   router.get('/webhook', (req, res) => {
     // Check if facebook passes us the correct verify token.
-    console.log(config);
 
     if (req.query['hub.verify_token'] === verifyToken) {
       res.send(req.query['hub.challenge']);
@@ -20,23 +19,22 @@ export default () => {
 
   router.post('/webhook', (req, res) => {
     // If facebook sends us an empty body, we just ignore it.
-    console.log(req);
     if (!req.body || _.isEmpty(req.body)) {
       return res.sendStatus(204);
     }
 
     const events = req.body.entry;
+
     _.forEach(events, (event) => {
       const pageId = event.id;
       const timestamp = event.time;
 
       const messages = event.messaging;
+
       _.forEach(messages, (message) => {
         const senderId = message.sender.id;
-
         const contents = message.message;
 
-        console.log('received something: ', contents)
         if (contents && contents.text) {
           console.log('sending Text message')
           sendTextMessage(senderId, contents.text);
